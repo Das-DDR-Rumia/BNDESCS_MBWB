0d6c031c6574ccd0dc68521b345616c713a0630b
marko1616 <marko1616@outlook.com>
2024-08-20 13:13:01 +0800

    Fix build.

--- misc files/module_info.py
+++
@@ -1,8 +0,0 @@
-
-# Point export_dir to the folder you will be keeping your module
-# Make sure you use forward slashes (/) and NOT backward slashes (\)
-
-export_dir = "F:/SteamLibrary/steamapps/common/MountBlade Warband/Modules/BNDESCSMBWB/"
-scan_module_textures = 1
-scan_module_sounds = 1
-#export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
